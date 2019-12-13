f08961c8b8cd438046c75a1a5a7f51f95e9a6295
Amos <amos.wu@madeforgoods.com.cn>
2019-12-13 18:10:35 +0800

    fix

---
+++ src/main/java/com/carry/原理系列/入门案例/HellWord.java
@@ -0,0 +1,9 @@
+package com.carry.原理系列.入门案例;
+
+public class HellWord {
+
+    public static void main(String[] args) {
+        HellWord hellWord = new HellWord();
+        System.out.println("Hello World");
+    }
+}
